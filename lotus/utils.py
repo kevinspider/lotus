6d630b5c6771cc9be58accb51f66f15ae4e04ff2
kevinspider <zhangyang.spider@gmail.com>
2025-04-10 09:27:51 +0800

    comment

--- lotus/utils.py
+++ lotus/utils.py
@@ -10,6 +10,7 @@ def merge_to_dict(keys: Iterable[Hashable], values: Iterable, strict:bool = True
 def dict_to_json(data: Dict, ensure_ascii=False, separators=(',', ':')):
     return json.dumps(data, ensure_ascii=ensure_ascii, separators=separators)
 
+# save data to pd
 def merge_data(data: list | dict, df: pd.DataFrame):
     if isinstance(data, list):
         tmp = pd.DataFrame(data)
